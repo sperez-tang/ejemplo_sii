e06b84a2977a92e96aeeb97094f87b869e1ab7b7
sperez <sperez@i-med.cl>
2022-05-24 16:31:58 -0400

    Cambio de puerto

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3040;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
